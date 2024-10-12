67709fb14a5426ebf0668a2237a08bfeda4944a2
AxelBilla <144888254+AxelBilla@users.noreply.github.com>
2024-10-12 08:22:16 +0200

    Update TD4_Bis_1.py

--- prog/4/TD4_Bis/TD4_Bis_1.py
+++ prog/4/TD4_Bis/TD4_Bis_1.py
@@ -1,4 +1,4 @@
-#Factorial Calc
+#Factorial Calc (gotta be a procedure)
 from TD4_Bis_func import *
 factInt = int(input("De quel nombre entier souhaitez-vous connaitre la factorielle?: "))
 bactorio(factInt)
