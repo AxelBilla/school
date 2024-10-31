c3653e46d3ea1678918b5486e6ddf220c5f76164
AxelBilla <144888254+AxelBilla@users.noreply.github.com>
2024-10-31 06:34:00 +0100

    Add files via upload

--- prog/5/5.4/5.4-2_func.cpp
+++ prog/5/5.4/5.4-2_func.cpp
@@ -15,31 +15,44 @@ int stockDisplay(vector<product> stockList) {
 product ProductAdd(string labelName) {
 	product received;
 	received.label = labelName;
-	cout << "Quantity: ";
-	cin >> received.amount;
-	cout << "Price: ";
-	cin >> received.buyPrice;
+	received.amount = -1; received.buyPrice = -1;
+	while (received.amount < 0) {
+		cout << "Quantity: ";
+		cin >> received.amount;
+	}
+	while (received.buyPrice < 0) {
+		cout << "Price: ";
+		cin >> received.buyPrice;
+	}
 	received.sellPrice = received.buyPrice * 1.56;
 	return received;
 }
 
 product StockAdd(product item,string labelName) {
-	int addedAmount;
-	float addedPrice;
-	cout << "Added Quantity: ";
-	cin >> addedAmount;
-	cout << "Added Price: ";
-	cin >> addedPrice;
-	item.buyPrice = (((item.buyPrice * item.amount) + (addedAmount * addedPrice)) / (item.amount + addedAmount));
-	item.sellPrice = item.buyPrice * 1.56;
+	int addedAmount = -1;
+	float addedPrice = -1;
+	while (addedAmount < 0) {
+		cout << "Added Quantity: ";
+		cin >> addedAmount;
+	}
+	while (addedPrice < 0) {
+		cout << "Added Price: ";
+		cin >> addedPrice;
+	}
+	if (addedPrice != 0) {
+		item.buyPrice = (((item.buyPrice * item.amount) + (addedAmount * addedPrice)) / (item.amount + addedAmount));
+		item.sellPrice = item.buyPrice * 1.56;
+	}
 	item.amount = item.amount + addedAmount;
 	return item;
 }
 
 product StockRem(product item) {
-	int soldAmount;
-	cout << "\nQuantity: ";
-	cin >> soldAmount;
+	int soldAmount = -1;
+	while (soldAmount < 0) {
+		cout << "\nQuantity: ";
+		cin >> soldAmount;
+	}
 	item.amount = item.amount - soldAmount;
 	return item;
 }
