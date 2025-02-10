bd3f3cc695884e72c05769fe21dd5bed1508084e
AxelBilla <144888254+AxelBilla@users.noreply.github.com>
2025-02-10 19:23:53 +0100

    Update deque.cpp

--- dev/C++ Misc/deque.cpp
+++ dev/C++ Misc/deque.cpp
@@ -10,7 +10,7 @@ int main() {
     rnd.push_back(2); //Juts like a vector
     
     deque<int>::iterator it; //Gotta declare its iterator's proprieties
-    for(it=rnd.begin(); it!=rnd.end(); ++it){ //Iterate through our deque's content. (.begin() & .end() returns an iterator, aka the one of the first and last slots)
+    for(it=rnd.begin(); it!=rnd.end(); ++it){ //Iterate through our deque's content. (.begin() & .end() returns an iterator, aka the one of the first and last slots) | Made by Axel B.
         cout << *it; //"it" takes the address of each "index" as it iterates (hence the pointer to show what's inside)
     }
     return 0;
